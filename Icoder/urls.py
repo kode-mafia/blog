20b8bc74da94c2e637fc345441fd6dc7f378cc4a
kode-mafia <dharmendramehta622@gmail.com>
2021-09-12 15:32:26 +0545

    loading completed

--- Icoder/urls.py
+++ Icoder/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
@@ -22,4 +23,6 @@ admin.site.index_title = "Welcome to Zapasi Blog Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('home.urls')),
+    path('blog/',include('blog.urls')),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    # return HttpResponse('This is blog page')  
+    context = {
+       'data': 'This is blog page'
+    }  
+    return render(request, 'home.html', context)
+
+def  blog(request):
+    return HttpResponse('This is blog page')    
